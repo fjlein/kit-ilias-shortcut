1765d97b85a8a3eb2a3a5200ad6bc3ffb8eafffa
fjlein <fjlein@gmail.com>
2024-12-05 15:54:37 +0100

    collapsable sections

--- src/lib/components/step.svelte
+++ src/lib/components/step.svelte
@@ -1,18 +1,29 @@
 <script lang="ts">
+    import * as Accordion from "$lib/components/ui/accordion/index.js";
     import type { Snippet } from "svelte";
-    interface Props {
+
+    let {
+        stepTitle,
+        children,
+    }: {
         children: Snippet;
         stepTitle: string;
-    }
-
-    let { stepTitle, children }: Props = $props();
+    } = $props();
 </script>
 
 <div>
-    <h3
-        class="step font-heading scroll-m-20 font-semibold tracking-tight text-xl mb-3"
-    >
-        {stepTitle}
-    </h3>
-    {@render children()}
+    <Accordion.Root type="single" class="w-full" value={"item"}>
+        <Accordion.Item value="item">
+            <Accordion.Trigger class="!pt-0">
+                <h3
+                    class="step font-heading scroll-m-20 font-semibold tracking-tight text-xl"
+                >
+                    {stepTitle}
+                </h3>
+            </Accordion.Trigger>
+            <Accordion.Content>
+                {@render children()}
+            </Accordion.Content>
+        </Accordion.Item>
+    </Accordion.Root>
 </div>
--- src/lib/components/steps.svelte
+++ src/lib/components/steps.svelte
@@ -9,7 +9,7 @@
 </script>
 
 <div
-    class="steps ml-2 sm:ml-4 border-l pl-8 [counter-reset:step] mr-4 [&>div:not(:last-child)]:mb-16"
+    class="steps ml-2 sm:ml-4 border-l pl-8 [counter-reset:step] mr-4 [&>div:not(:last-child)]:mb-12"
 >
     {@render children()}
 </div>
